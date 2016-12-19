73d0122bd2da9dab20b96aa81688c68d3d3bdf54
Harry Wolff <hswolff@gmail.com>
2016-12-18 21:57:56 -0500

    chore: init commit

---
+++ spec/tree-view-open-files-plus-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TreeViewOpenFilesPlusView from '../lib/tree-view-open-files-plus-view';
+
+describe('TreeViewOpenFilesPlusView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
